b9c438c7dfe26595e7eb0b94c3fe093141a51aae
James Wyn <thisisjameswyn@gmail.com>
2021-02-12 10:26:11 -0800

    add initial logic and API call, intial HTML setup

---
+++ src/exchangeCalc.js
@@ -0,0 +1,14 @@
+export default class ExchangeCalc {
+  static getExRate(firstCur, secondCur) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${firstCur}/${secondCur}`, { mode: 'cors' })
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
